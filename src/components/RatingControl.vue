4c761d8926e4e80a7901bc495d9998d41fd3f312
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-02-25 16:10:43 -0500

    Use emit

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'good' }">
+      <button type="button" @click="activate('good')">Good</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:ModelValue'],
+  methods: {
+    activate(option) {
+      this.$emit('update:ModelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: purple;
+}
+
+.active button {
+  color: purple;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,16 +1,23 @@
 <template>
-  <form>
-    <div class="form-control">
+  <form @submit.prevent="submit">
+    <div class="form-control" :class="{ invalid: !valid }">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="username"
+        @blur="validateUsername"
+      />
+      <p v-if="!valid">Please enter proper username.</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="age" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,38 +26,108 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          v-model="interest"
+          value="news"
+        />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          v-model="interest"
+          value="tutorials"
+        />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          v-model="interest"
+          value="nothing"
+        />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input
+          id="how-video"
+          name="how"
+          type="radio"
+          v-model="how"
+          value="video"
+        />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input
+          id="how-blogs"
+          name="how"
+          type="radio"
+          v-model="how"
+          value="blogs"
+        />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input
+          id="how-other"
+          name="how"
+          type="radio"
+          v-model="how"
+          value="other"
+        />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div>
       <button>Save Data</button>
     </div>
   </form>
 </template>
+
+<script>
+import RatingControl from './RatingControl.vue';
+
+export default {
+  components: {
+    RatingControl,
+  },
+  data() {
+    return {
+      username: '',
+      age: null,
+      referrer: 'wom',
+      interest: [],
+      how: null,
+      valid: 'pending',
+      rating: '',
+    };
+  },
+  methods: {
+    submit() {},
+    validateUsername() {
+      if (this.username === '') {
+        this.valid = false;
+      } else {
+        this.valid = true;
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
@@ -64,6 +141,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  border-color: red;
 }
 
 label {
